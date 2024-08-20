f80836f83366e3a227af732fcce1cf4fb6b9a800
Jkhall81 <jason.kei.hall@gmail.com>
2024-08-19 19:54:44 -0700

    added stuff for labs and documents in the dashboard.  three sections left.

--- app/(root)/dashboard/labs/page.tsx
+++ app/(root)/dashboard/labs/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { usePatientStore } from "@/lib/store";
+import { LabsResultsList } from "@/components/DashboardLabs/LabsResultsList";
 
 const LabsPage = () => {
-  const selectedPatient = usePatientStore((state) => state.selectedPatient);
   const relatedData = usePatientStore((state) => state.relatedData);
   return (
     <section className="w-full h-full flex flex-col">
       <div className="w-full h-full">
+        <LabsResultsList data={relatedData.labs} />
         <div className="h-[100px]" />
       </div>
     </section>
---
+++ components/DashboardLabs/LabsResultsList.tsx
@@ -0,0 +1,33 @@
+import { Lab } from "@/lib/dummyData/types";
+import { LabsResultsItem } from "./LabsResultsItem";
+
+interface Props {
+  data: Lab[];
+}
+
+export const LabsResultsList = ({ data }: Props) => {
+  return (
+    <section className="border-2 border-neutral-300 dark:border-white mt-6 rounded-3xl">
+      <h2 className="text-4xl text-center mt-6 font-semibold">Labs</h2>
+      {data.map((x, index) => (
+        <LabsResultsItem
+          key={index}
+          index={index}
+          encounterNumber={x.encounterNumber}
+          labName={x.labName}
+          testName={x.testName}
+          result1={x.result1}
+          result2={x.result2}
+          result3={x.result3}
+          result4={x.result4}
+          result5={x.result5}
+          result6={x.result6}
+          result7={x.result7}
+          result8={x.result8}
+          testDate={x.testDate}
+          resultDate={x.resultDate}
+        />
+      ))}
+    </section>
+  );
+};
